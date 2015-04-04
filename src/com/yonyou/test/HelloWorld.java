b1a4278a50344536fca9fbbb4a4e66f1f18f66b0
zhengws <zwsbuaa@163.com>
2015-04-04 18:00:41 +0800

    首次提交到本地仓库

---
+++ src/com/yonyou/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.yonyou.test;
+
+public class HelloWorld {
+
+	/**
+	 * 
+	 * @param args
+	 *            Date: 2015年4月4日 User:Administrator
+	 * @Date:2015年4月4日下午5:56:13
+	 */
+	public static void main(String[] args) {
+		System.out.println("sdfas");
+
+	}
+}
