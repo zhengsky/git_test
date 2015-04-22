cb6a0e1b2ad28ebb57805cf8971933f3207ce00b
zhengwsa <zhengwsa@yonyou.com>
2015-04-22 16:59:12 +0800

    "测试使用"

---
+++ src/com/yonyou/test/HelloWorld2.java
@@ -0,0 +1,18 @@
+package com.yonyou.test;
+
+public class HelloWorld {
+
+	/**
+	 * 
+	 * @param args
+	 * Date: 2015年4月4日
+	 * User:Administrator
+	 * @Date:2015年4月4日下午6:15:16
+	 */
+	public static void main(String[] args) {
+		System.out.println("sdfas");
+
+		System.out.println("Second");
+		
+	}
+}
