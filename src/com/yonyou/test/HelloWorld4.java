e5d06f0553765599a2aed0828db154a8acc1912f
zhengws <zwsbuaa@163.com>
2015-08-16 22:40:46 +0800

    程序

---
+++ src/com/yonyou/test/HelloWorld4.java
@@ -0,0 +1,17 @@
+package com.yonyou.test;
+
+public class HelloWorld4 {
+
+	/**
+	 * 
+	 * @param args
+	 * Date: 2015å¹?æœ?æ—?	 * User:Administrator
+	 * @Date:2015å¹?æœ?æ—¥ä¸‹å?:15:16
+	 */
+	public static void main(String[] args) {
+		System.out.println("sdfas");
+
+		System.out.println("Secondååˆ†ä¸‰ä»½");
+		
+	}
+}
